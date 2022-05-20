5a73fc3c21e2204dd3e6ae4d318bb6a5f448c69e
Pierce McBride <piercermcbride@gmail.com>
2022-05-20 10:00:38 -0400

    Added comments to IRemoteInputProvider

--- Runtime/IRemoteInputProvider.cs
+++ Runtime/IRemoteInputProvider.cs
@@ -4,13 +4,24 @@ namespace Futurus.RemoteInput
 {
     public interface IRemoteInputProvider
     {
+        /// <summary>
+        /// Enforces that the interface is either implemented on a Monobehavior or on an object with access to one
+        /// </summary>
         public Transform transform { get; }
 
         /// <summary>
         /// Whether or not to perform additional Physics.Raycast to make sure there's no 3D objects blocking the remote input 
         /// </summary>
         public bool CheckOcclusion { get; }
+
+        /// <summary>
+        /// Used if CheckOcclusion is set to true to find blocking physics layers
+        /// </summary>
         public LayerMask BlockingOcclusionMask { get; }
+
+        /// <summary>
+        /// The maximum distance from the provider that will be considered for remote inpuit
+        /// </summary>
         public float MaxDistance { get; }
 
         /// <summary>
